af88ebfb11a6d55323027632e4408b1c8685de38
DamienLacombe <55766706+DamienLacombe@users.noreply.github.com>
2022-07-31 19:26:06 +0200

    initialisation projet, changement composent onclick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Mosaic from "./components/Mosaic";
+import Slider from "./components/Slider";
 
 function App() {
+
+  const [slider, setSlider] = useState(true);
+  const [btnContent, setBtnContent] = useState("Mosaic"); 
+
+  function btnClick() {
+    setSlider(!slider);
+    btnContent === "Slider" ? setBtnContent("Mosaic") : setBtnContent("Slider");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => btnClick() }>{btnContent}</button>
+        {
+          slider === true ? <Slider></Slider> : <Mosaic></Mosaic>
+        }
     </div>
   );
+
 }
 
 export default App;
